af3b626cc7ce1e5927d169cc48adf8a8d50eecd3
Jim Grady <jimgrady.jg@gmail.com>
2021-08-20 14:25:31 -0400

    Update from review

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #! /bin/bash
 
-docker build -t sillsdev/aws-kubectl -f Dockerfile .
-docker push sillsdev/aws-kubectl
+#####################################################
+# build.sh - script to build and push the
+# sillsdev/aws-kubectl image to Docker Hub
+#
+# Usage:
+#   build.sh [ tag1, tag2, ...]
+#
+# build.sh will build the sillsdev/aws-kubectl as
+# an untagged image and push it to Docker Hub.  (Docker Hub
+# will assign this image the tag "latest")
+# It will then for each tag provided as a command argument, it will:
+#   - add the tag to the latest image that was just built
+#   - push the image to Docker Hub
+
+IMAGE=sillsdev/aws-kubectl
+docker build -t ${IMAGE} -f Dockerfile .
+docker push ${IMAGE}
 while [ $# -gt 0 ] ; do
-  docker tag sillsdev/aws-kubectl:latest sillsdev/aws-kubectl:$1
-  docker push sillsdev/aws-kubectl:$1
+  docker tag ${IMAGE}:latest ${IMAGE}:$1
+  docker push ${IMAGE}:$1
   shift
 done
