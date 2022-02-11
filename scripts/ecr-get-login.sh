59513856804c161cb96ed2900053d2233a94b5f9
Jim Grady <jimgrady.jg@gmail.com>
2022-02-11 12:56:11 -0500

    Quote Docker email and password variables

--- scripts/ecr-get-login.sh
+++ scripts/ecr-get-login.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 set -e
 
-echo "Retrieving Docker Credentials for the AWS ECR Registry ${AWS_ACCOUNT}"
 DOCKER_REGISTRY_SERVER=${AWS_ACCOUNT}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 DOCKER_USER=AWS
+echo "Retrieving Docker Credentials for the AWS ECR Registry ${AWS_ACCOUNT}"
+echo "AWS_DEFAULT_REGION is ${AWS_DEFAULT_REGION}"
+echo "DOCKER_REGISTRY_SERVER is ${DOCKER_REGISTRY_SERVER}"
 DOCKER_PASSWORD=`aws ecr get-login-password --region ${AWS_DEFAULT_REGION}`
 
 for namespace in ${NAMESPACES}
@@ -18,8 +20,8 @@ do
 	kubectl create secret docker-registry ${PULL_SECRET_NAME} \
 		--docker-server=$DOCKER_REGISTRY_SERVER \
 		--docker-username=$DOCKER_USER \
-		--docker-password=$DOCKER_PASSWORD \
-		--docker-email=${DOCKER_EMAIL} \
+		--docker-password="$DOCKER_PASSWORD" \
+		--docker-email="${DOCKER_EMAIL}" \
 		--namespace=${namespace}
 done
 
