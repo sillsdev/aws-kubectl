9b70a131e901f90bb47a19b57a420935fd39cd85
D. Ror. <imnasnainaec@gmail.com>
2024-11-15 17:08:33 -0500

    Support arm64 (#5)

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,20 @@
+# Resulting image is stored in https://gallery.ecr.aws/thecombine/aws-kubectl
+# See https://github.com/sillsdev/aws-kubectl#readme for usage information
 FROM ubuntu:22.04
-# Resulting image is stored in https://hub.docker.com/r/sillsdev/aws-kubectl
-# See https://github.com/sillsdev/aws-kubectl#readme for usage information.
-LABEL maintainer="Jim Grady <jimgrady.jg@gmail.com>"
+LABEL maintainer="Danny Rorabaugh <daniel_rorabaugh@sil.org>"
+
+# Use the <arch> from the --platform=<os>/<arch> flag
+ARG TARGETARCH
 
 # Install AWS-CLI version 2
 # See https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
-RUN apt-get update && \
+RUN AWS_ARCH=$(case $TARGETARCH in amd64) echo x86_64;; arm64) echo aarch64;; *) exit 1;; esac) && \
+  apt-get update && \
   apt-get install -y apt-utils curl zip && \
   apt-get autoremove && \
   apt-get clean && \
   rm -rf /var/lib/apt/lists/* && \
-  curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip && \
+  curl -sL https://awscli.amazonaws.com/awscli-exe-linux-${AWS_ARCH}.zip -o awscliv2.zip && \
   unzip awscliv2.zip && \
   aws/install && \
   rm -rf awscliv2.zip \
@@ -21,7 +25,7 @@ RUN apt-get update && \
 
 # Install kubectl
 # See https://kubernetes.io/docs/tasks/tools/
-RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${TARGETARCH}/kubectl" \
   && mv kubectl /usr/local/bin \
   && chmod +x /usr/local/bin/kubectl
 
--- build.sh
+++ build.sh
@@ -1,24 +1,52 @@
 #! /bin/bash
 
-#####################################################
-# build.sh - script to build and push the
-# sillsdev/aws-kubectl image to Docker Hub
+######################################################################
+# build.sh - script to build and push the aws-kubectl image
 #
 # Usage:
-#   build.sh [ tag1, tag2, ...]
+#   build.sh -v/--version VERSION [-a/--arch ARCH] [-i/--image IMAGE]
 #
-# build.sh will build the sillsdev/aws-kubectl as
-# an untagged image and push it to Docker Hub.  (Docker Hub
-# will assign this image the tag "latest")
-# For each tag provided as a command argument, it will:
-#   - add the tag to the latest image that was just built
-#   - push the image to Docker Hub
+# Supported architectures: amd64, arm64
+# Default architecture: amd64
+# Default image: public.ecr.aws/thecombine/aws-kubectl
+######################################################################
 
-IMAGE=sillsdev/aws-kubectl
-docker build -t ${IMAGE} -f Dockerfile .
-docker push ${IMAGE}
-while [ $# -gt 0 ] ; do
-  docker tag ${IMAGE}:latest ${IMAGE}:$1
-  docker push ${IMAGE}:$1
+# Default arguments
+ARCH=amd64
+IMAGE="public.ecr.aws/thecombine/aws-kubectl"
+VERSION=
+
+# Parse arguments to customize installation
+while (( "$#" )) ; do
+  OPT=$1
+  case $OPT in
+    -a|--arch)
+      ARCH=$2
+      shift
+      ;;
+    -i|--image)
+      IMAGE=$2
+      shift
+      ;;
+    -v|--version)
+      VERSION=$2
+      shift
+      ;;
+    *)
+      warning "Unrecognized option: $OPT"
+      ;;
+  esac
   shift
 done
+
+if [ -z "${VERSION}" ]; then
+  echo "-v/--version required"
+  exit 1
+fi
+
+echo "Image: ${IMAGE}"
+echo "Tag: ${VERSION}-${ARCH}"
+IMAGE=${IMAGE}:${VERSION}-${ARCH}
+
+echo "Building and pushing"
+docker build --platform=linux/${ARCH} --push -t ${IMAGE} .
